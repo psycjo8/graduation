ebd7a4afc83a28f703238c937d02dea4a77fbd21
seongeun Chae <psycjo8@gmail.com>
2018-03-25 02:46:16 +0900

    콘솔에서 알림 보내기 추가

--- app/src/main/java/kr/ac/kpu/block/smared/TabActivity.java
+++ app/src/main/java/kr/ac/kpu/block/smared/TabActivity.java
@@ -3,6 +3,7 @@ package kr.ac.kpu.block.smared;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,11 +11,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Hashtable;
+
 public class TabActivity extends AppCompatActivity  {
 
     Fragment fragment;
 
     long lastPressted;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseUser user;
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,6 +58,15 @@ public class TabActivity extends AppCompatActivity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        Hashtable<String, Object> token = new Hashtable<>();
+        token.put("fcmToken", FirebaseInstanceId.getInstance().getToken());
+        myRef.child(user.getUid()).updateChildren(token);
 
 
         FragmentManager fragmentManager = getFragmentManager();
